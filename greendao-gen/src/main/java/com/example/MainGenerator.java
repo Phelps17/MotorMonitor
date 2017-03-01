84ed8b93dcfcf2e1da78081db45dabf194e55951
Phelps17 <Phelps3@wisc.edu>
2017-03-01 01:48:37 -0600

    added object relational mapping

---
+++ greendao-gen/src/main/java/com/example/MainGenerator.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Property;
+import de.greenrobot.daogenerator.Schema;
+
+public class MainGenerator {
+    private static final String PROJECT_DIR = System.getProperty("user.dir");
+
+    public static void main(String[] args) {
+        Schema schema = new Schema(1, "com.example.prekode");
+        schema.enableKeepSectionsByDefault();
+
+        addTables(schema);
+
+        try {
+            new DaoGenerator().generateAll(schema, PROJECT_DIR + "/app/src/main/java");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void addTables(final Schema schema) {
+        //TODO add database later
+    }
+}
