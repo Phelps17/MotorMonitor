e24ce37a465c26090c8d8c8cfb88ef4e53637614
Phelps17 <Phelps3@wisc.edu>
2017-03-09 02:00:42 -0600

    incorporated graphs into views

--- app/src/main/java/com/tylerphelps/motormonitor/ModuleScreenAdapter.java
+++ app/src/main/java/com/tylerphelps/motormonitor/ModuleScreenAdapter.java
@@ -5,9 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.TextView;
-
 import java.util.ArrayList;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 /**
  * Created by TylerPhelps on 3/4/17.
@@ -33,6 +35,18 @@ public class ModuleScreenAdapter extends ArrayAdapter<Integer> {
             viewHolder = new ViewHolder();
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.module_main_view, parent, false);
+            convertView.setMinimumHeight(parent.getMeasuredHeight());
+
+            GraphView graph = (GraphView) convertView.findViewById(R.id.graph);
+
+            LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                    new DataPoint(0, 1),
+                    new DataPoint(1, 5),
+                    new DataPoint(2, 3),
+                    new DataPoint(3, 2),
+                    new DataPoint(4, 6)
+            });
+            graph.addSeries(series);
 
             convertView.setTag(viewHolder);
         } else {
